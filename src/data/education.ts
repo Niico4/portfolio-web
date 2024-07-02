3a86cfafd065fd14a95fef9a972611b322f455d0
“Niico4” <nicogarzon131@gmail.com>
2024-07-02 18:43:50 -0500

    add certification css modern

--- src/data/education.ts
+++ src/data/education.ts
@@ -10,6 +10,10 @@ export const certifications = [
     url: 'https://www.udemy.com/certificate/UC-caa20bb8-b619-4753-96cf-89b04c307808/',
   },
   {
+    image: '/certificado-css-2.webp',
+    url: 'https://www.udemy.com/certificate/UC-3a832df6-9939-459b-ba51-da83b8675501/',
+  },
+  {
     image: '/certificado-js.webp',
     url: 'https://www.udemy.com/certificate/UC-d99307af-48a3-46d2-9587-0fadd316a52d/',
   },
